26ed8428a0a530017880d936c1b7dde03dead7cd
Parinaz Soltani <p.soltani007@gmail.com>
2022-02-05 20:26:08 +0100

    create recyclerView adapter

--- app/src/main/java/com/ananayis/affirmaions/MainActivity.kt
+++ app/src/main/java/com/ananayis/affirmaions/MainActivity.kt
@@ -3,6 +3,8 @@ package com.ananayis.affirmaions
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ananayis.affirmaions.adapter.ItemAdapter
 import com.ananayis.affirmaions.data.DataSource
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,17 @@ class MainActivity : AppCompatActivity() {
 
 //        val textView: TextView = findViewById(R.id.textView)
 //        textView.text = DataSource().loudAffirmation().size.toString()
+
+
+        // Initialize data.
+        val myDataset = DataSource().loudAffirmation()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
     }
 }
+
