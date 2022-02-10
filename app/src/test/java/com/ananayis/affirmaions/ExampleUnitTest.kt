d22182b7c464160a2836e0b43e1a1b77fbc792ee
Parinaz Soltani <p.soltani007@gmail.com>
2022-02-10 14:23:30 +0100

    unit test

--- app/src/test/java/com/ananayis/affirmaions/ExampleUnitTest.kt
+++ app/src/test/java/com/ananayis/affirmaions/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package com.ananayis.affirmaions
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.runner.manipulation.Ordering
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +11,12 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    private val context = mock(Ordering.Context::class.java)
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+
 }
